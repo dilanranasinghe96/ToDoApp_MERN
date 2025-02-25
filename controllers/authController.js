1c38bd34181e8e3fae2111c39ade43460ab31c14
Dilan Bandara Ranasinghe <1128dilan@gmail.com>
2025-02-25 21:06:34 +0530

    changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,59 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const secretKey = process.env.JWT_SECRET;
+
 async function registerUser(req,res){
-    let data = req.body;
+    let {firstName,lastName,username,password} = req.body;
+    try {
+        const duplicate = await User.find({username});
+        if(duplicate && duplicate.length>0){
+            return res.status(400).send('User already registered with this username');
+        }
+        let user = new User({firstName,lastName,username,password});
+        const result = await user.save();
+        console.log(result);
+        res.status(201).send('User registered successfully!');
+        
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error);
+    }    
     
-    res.send('Success');
+}
+
+async function loginUser(req,res){
+    
+    try {
+        const {username,password} = req.body;
+        const user = await User.findOne({username});
+        if(!user){
+            return res.status(404).send({error:'Authentication Failed!'});            
+        }
+        const isPasswordValid = await user.comparePassword(password);
+        if(!isPasswordValid){
+            return res.status(404).send({error:'Wrong Password'});    
+        }
+        let token = jwt.sign({userId:user?._id},secretKey,{expiresIn:'1h'});
+        let finalData={
+            userId:user?._id,
+            username:user?.username,
+            firstName:user?.firstName,
+            lastName:user?.lastName,
+            token
+        }
+        res.send(finalData);
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error);
+    }    
+    
 }
 
 const AuthController = {
-    registerUser
+    registerUser,
+    loginUser
 }
 
 module.exports = AuthController;
--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
  require('dotenv').config();
- const authRoutes = require('./routes/authRoutes')
+ const authRoutes = require('./routes/authRoutes');
+ const toDoRoutes = require('./routes/ToDoRoutes')
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api',authRoutes);
+app.use('/api/todo',toDoRoutes);
 
 mongoose.connect(process.env.DB_URL).then(() => {
     console.log('Connected to MongoDB');
